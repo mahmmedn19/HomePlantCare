f2c8ffd2e880599b149c4f710bae471e2e2c4b69
Mohamed Naser <m.nasser@trips.sa.com>
2025-04-12 22:57:05 +0200

    feat(DiseasesAdapter): implement Base64 image decoding for disease items

--- app/src/main/java/com/project/homeplantcare/ui/plant_details/DiseasesAdapter.java
+++ app/src/main/java/com/project/homeplantcare/ui/plant_details/DiseasesAdapter.java
@@ -1,10 +1,13 @@
 package com.project.homeplantcare.ui.plant_details;
 
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-import com.project.homeplantcare.databinding.ItemDiseaseCardBinding;
+import com.project.homeplantcare.R;
 import com.project.homeplantcare.data.models.DiseaseItem;
+import com.project.homeplantcare.data.utils.ImageUtils;
+import com.project.homeplantcare.databinding.ItemDiseaseCardBinding;
 import com.project.homeplantcare.ui.base.BaseAdapter;
 
 import java.util.List;
@@ -17,18 +20,35 @@ public class DiseasesAdapter extends BaseAdapter<DiseaseItem, ItemDiseaseCardBin
         super(itemList);
         this.diseaseList = itemList;
     }
+
     public void setDiseases(List<DiseaseItem> diseases) {
         this.diseaseList = diseases;
         notifyDataSetChanged(); // Notify adapter about the data change
     }
+
     @Override
     public ItemDiseaseCardBinding createBinding(LayoutInflater inflater, ViewGroup parent, boolean attachToParent) {
         return ItemDiseaseCardBinding.inflate(inflater, parent, attachToParent);
     }
+
     @Override
     public void onBindViewHolder(BaseViewHolder<ItemDiseaseCardBinding> holder, int position, DiseaseItem currentItem) {
         ItemDiseaseCardBinding binding = holder.binding;
         binding.setDisease(currentItem);
+
+        // Decode Base64 string to Bitmap and set to ImageView
+        String base64Image = currentItem.getImageResId();
+        if (base64Image != null && !base64Image.isEmpty()) {
+            try {
+                Bitmap bitmap = ImageUtils.decodeBase64ToImage(base64Image);
+                binding.imgDisease.setImageBitmap(bitmap);
+            } catch (IllegalArgumentException e) {
+                // Handle invalid Base64 string (set a placeholder image)
+                binding.imgDisease.setImageResource(R.drawable.upload_image);
+            }
+        } else {
+            binding.imgDisease.setImageResource(R.drawable.upload_image);
+        }
         binding.executePendingBindings();
     }
 
